7ac4685aea3f6acf5ad540e358b6d2eba28f5993
Happy <104614758+happyprajapati@users.noreply.github.com>
2024-08-28 14:51:43 +0530

    update user.jsx

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -1,5 +1,5 @@
 import Breadcrumb from '../components/Breadcrumbs/Breadcrumb';
-import TableOne from '../components/tables/TableOne';
+import TableOne from '../components/Tables/TableOne';
 import DefaultLayout from '../layout/DefaultLayout';
 
 export default function User(){

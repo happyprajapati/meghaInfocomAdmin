09202990e6d3aa0109e4fb4c0ba8875d053d1405
Happy <104614758+happyprajapati@users.noreply.github.com>
2024-08-28 15:00:52 +0530

    update product.jsx file

--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -1,7 +1,7 @@
 import Breadcrumb from '../components/Breadcrumbs/Breadcrumb';
 import DefaultLayout from '../layout/DefaultLayout';
 import Buttons from '../components/buttons';
-import TableOne from '../components/Tables/TableOne.js';
+import TableOne from '../components/Tables/TableOne';
 
 export default function Product(){
   return (
